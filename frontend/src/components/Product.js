728d6678356db757ce74f2aed9b07000a12d7528
oneanonymousdeveloper <121730262+oneanonymousdeveloper@users.noreply.github.com>
2025-03-24 20:15:35 +0530

    homescreen-setup

---
+++ frontend/src/components/Product.js
@@ -0,0 +1,26 @@
+import { Card } from "react-bootstrap";
+
+const Product = ({ product }) => {
+  return (
+    <Card className="my-3 p-3 rounded">
+      <a href={`/product${product._id}`} >
+        <Card.Img src={product.image} />
+        <Card.Body>
+          <Card.Title>
+            <strong>{product.name}</strong>
+          </Card.Title>
+          <Card.Text as="div">
+            <div className="my-3">
+              {product.rating} from {product.numReviews} reviews
+            </div>
+          </Card.Text>
+          <Card.Text as='h3'>
+            ${[product.price]}
+          </Card.Text>
+        </Card.Body>
+      </a>
+    </Card>
+  );
+};
+
+export default Product;
