369419be7561fd5bf763947d048ce4c0d0fc329d
oneanonymousdeveloper <121730262+oneanonymousdeveloper@users.noreply.github.com>
2025-03-26 18:22:27 +0530

    image-dynamicfetching-setup

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -119,7 +119,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+MEDIA_URL= '/images/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'static'
+
+]
+
+MEDIA_ROOT = BASE_DIR / 'static/images'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
@@ -127,3 +135,4 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOW_ALL_ORIGINS = True
+
