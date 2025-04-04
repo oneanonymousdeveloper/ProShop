33741f6fb97c9f87ada610baa8a8a1f237d5b857
oneanonymousdeveloper <121730262+oneanonymousdeveloper@users.noreply.github.com>
2025-04-04 13:47:15 +0530

    commit

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -136,7 +136,9 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 #     }
 # else:
 DATABASES = {
-    'default': dj_database_url.config(default=f'sqlite:///{BASE_DIR}/db.sqlite3', conn_max_age=600)
+'default': dj_database_url.config(
+conn_max_age=600
+)
 }
 
 # Password validation
